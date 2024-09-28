ead5e9c2c80b8753c5df1c04bd9e5241cd5a7962
Raghavan <raghavanmarch2002@gmail.com>
2024-09-28 14:00:50 +0530

    track clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('a[href^="http"]')
+
     links.forEach((link) => {
+        // open in new tab
         link.setAttribute("target", "_blank")
+
+        // track clicks
+        link.addEventListener("click", () => {
+            gtag("event", "click", {
+                event_category: "link",
+                event_label: link.getAttribute("href"), // Use the href as the event label
+            })
+        })
     })
 })
